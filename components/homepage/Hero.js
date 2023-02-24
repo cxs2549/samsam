52e5be1ea881b4116e20059004fae2570db3699c
cxs2549 <sanchez.cliff.86@gmail.com.com>
2023-02-24 14:20:33 -0800

    add hero

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react"
+import { Gloock } from "next/font/google"
+const gloock = Gloock({ subsets: ["latin"], weight: "400" })
 const Hero = () => {
   return (
     <div>
-        <img src="./assets/sam.png" alt="" />
+      <img src="./assets/sam.png" alt="" />
+      <div class="container p-4 bg-neutral-50" >
+        <div
+          id="title"
+          style={{ backgroundImage: 'url("./assets/herobg.png")' }}
+        >
+          <h1 className="leading-[.9] font-extrabold">Healthy Living</h1>
+        </div>
+        <div className="mt-1 text-neutral-600 dark:text-neutral-100">
+          <p className={gloock.className}>with Samantha Sambilé</p>
+        </div>
+      </div>
     </div>
   )
 }
