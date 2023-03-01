ef5c9942b0d3d718a16306fa47872252b2f6e6d8
cxs2549 <sanchez.cliff.86@gmail.com.com>
2023-02-28 22:13:35 -0800

    rearrange hero

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -1,19 +1,27 @@
 import React from "react"
-import { Gloock } from "next/font/google"
-const gloock = Gloock({ subsets: ["latin"], weight: "400" })
+import { Dancing_Script } from "next/font/google"
+const dancingScript = Dancing_Script({ subsets: ["latin"], weight: "400" })
 const Hero = () => {
   return (
     <div className="mb-8 relative bg-white dark:bg-black z-40">
-      <div class="container p-4 pt-12">
-        <div
-          id="title"
-          style={{ backgroundImage: 'url("./assets/herobg.png")' }}
-        >
-          <h1 className="leading-[.8] font-extrabold">Professional Organzier</h1>
-        </div>
-        
-      </div>
       <img src="./assets/sam.png" alt="" />
+      <div class="container px-4 pt-8">
+        <div id="title">
+          <div className="h-[200px] dark:text-white">
+            <h1 className={`${dancingScript.className}`}>Professional</h1>
+            <div className="-translate-y-6">
+              <h1 className={dancingScript.className}>Organizer</h1>
+            </div>
+            <div className="-translate-y-10">
+              <h1 className={dancingScript.className}>&</h1>
+            </div>
+            <div className="-translate-y-14">
+              <h1 className={dancingScript.className}>Life Coach</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+     
     </div>
   )
 }
