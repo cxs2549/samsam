0c44efc27212c8539e9997df05c9e936a8204a82
cxs2549 <sanchez.cliff.86@gmail.com.com>
2023-03-03 06:12:51 -0800

    add masonry hero

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -5,14 +5,37 @@ const dancingScript = Dancing_Script({ subsets: ["latin"], weight: "400" })
 const cormorant = Cormorant({ subsets: ["latin"], weight: "400" })
 const Hero = () => {
   return (
-    <div className="pt-4 pb-6 px-4 flex flex-col">
-     <img src="./assets/sam.png" alt="" className="rounded-full w-24 h-24 object-cover" />
-      <div className="">
-        <h1 className={dancingScript.className}>
-          Professional Organizer
-        </h1>
-        <p className={cormorant.className}>Los Angeles, CA</p>
+    <div className="flex flex-wrap px-4 py-8 gap-2">
+      <div className=" flex flex-col flex-1">
+        <img
+          src="./assets/sam.png"
+          alt=""
+          className="rounded-full w-24 h-24 object-cover"
+        />
+        <div className="">
+          <h1 className={dancingScript.className}>Professional Organizer</h1>
+          <p className={cormorant.className}>Los Angeles, CA</p>
+        </div>
       </div>
+
+      <div className="flex-1 rounded overflow-hidden">
+        <div>
+          <img
+            src="./assets/closet.png"
+            className="h-[80px] w-full object-cover object-top rounded"
+            alt=""
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-2 mt-2">
+          <img src="./assets/vanity.png" alt="" className="rounded" />
+          <img src="./assets/veggies.jpg" className="h-full w-full object-cover rounded" alt="" />
+        </div>
+      </div>
+      <img
+        src="./assets/closet.png"
+        className="h-[120px] object-cover w-full object-bottom"
+        alt=""
+      />
     </div>
   )
 }
